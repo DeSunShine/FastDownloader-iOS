5b48fefebf1fc514b115fbf25e5214596fa5a377
DeSunShine <87091315+DeSunShine@users.noreply.github.com>
2026-09-23 10:23:13 +0300

    Handle foreground and tapped download notifications

--- FastDownloader/FastDownloaderApp.swift
+++ FastDownloader/FastDownloaderApp.swift
@@ -1,7 +1,16 @@
 import SwiftUI
 import UIKit
+import UserNotifications
 
-final class AppDelegate: NSObject, UIApplicationDelegate {
+final class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
+        return true
+    }
+
     func application(
         _ application: UIApplication,
         handleEventsForBackgroundURLSession identifier: String,
